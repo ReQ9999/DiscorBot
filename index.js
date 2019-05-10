46b2fbcf2d050380c1c0d962cfbf17c79da514f8
ReQ9999 <50496242+ReQ9999@users.noreply.github.com>
2019-05-10 19:37:33 +0200

    Delete index.js

--- index.js
+++
@@ -1,1 +0,0 @@
-const Discord = require ('discord.js'); const client = new Discord.Client (); client.on ("ready", () => console.log ("Zalogowano jako $ client.user.tag!");); client.on ('message', msg => if (msg.content === 'ping') msg.reply ('pong');); client.login ("NTc2NDMzNjMzMzg5MTgyOTg2.XNWdzQ.xFk3poNlqlFvtlcRSmAqK7LXzNQ");
